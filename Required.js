3f597758f29ad1ba0477ec03c6114e277d611b23
Anton Pysarenko <anton1980.00@gmail.com>
2020-07-27 18:33:26 +0300

    use setTimeout

--- Required.js
+++ Required.js
@@ -1,4 +1,19 @@
 let elem = document.body.appendChild(document.createElement('h3'))
+function clock (time) {
+  setTimeout(
+    function() {
+      let date = new Date()
+      elem.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+      return time === 0 ? elem : clock(-- time)
+    },1000
+  )
+}
+
+clock(5)
+
+
+
+/*et elem = document.body.appendChild(document.createElement('h3'))
 let time = 5
 const clock = setInterval(() => {
   if (time >= 0) {
@@ -6,4 +21,4 @@ const clock = setInterval(() => {
     elem.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
     time--
   } else clearInterval(time) 
-},1000)
+},1000)*/
